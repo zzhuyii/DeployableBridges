a127264b36354a3dd4f06fe885016b262d68de87
Yi Zhu <37033716+zzhuyii@users.noreply.github.com>
2026-04-19 11:18:08 -0400

    Update

--- improved scissor/LRFD Load/Scissor_Bridge_2_Load.py
+++
@@ -1,5 +0,0 @@
-from Scissor_Bridge_2_Pedestrain_Load_LRFD import main
-
-
-if __name__ == "__main__":
-    main()
--- scissor/LRFD Load/Scissor_Bridge_Load.py
+++
@@ -1,5 +0,0 @@
-from Scissor_Bridge_Pedestrain_Load_LRFD import main
-
-
-if __name__ == "__main__":
-    main()
